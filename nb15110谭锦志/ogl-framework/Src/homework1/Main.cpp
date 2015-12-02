e9961fcca2c04df5af795bbea4cae7409cd6ee5c
apanoo <apanoo@126.com>
2015-12-02 15:34:38 +0800

    创建项目，添加freeglut,glew,glm,gltools库

---
+++ nb15110谭锦志/ogl-framework/Src/homework1/Main.cpp
@@ -0,0 +1,17 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include "Main.h"
+
+//link to libraries
+//#pragma comment(lib, "opengl32.lib")
+//#pragma comment(lib, "glu32.lib")
+//#pragma comment(lib, "winmm.lib")
+
+//WinMain
+int WINAPI WinMain(	HINSTANCE	hInstance,			//Instance
+					HINSTANCE	hPrevInstance,		//Previous Instance
+					LPSTR		lpCmdLine,			//Command line params
+					int			nShowCmd)			//Window show state
+{
+	return 0;
+}
---
+++ nb15110谭锦志/ogl-framework/Src/homework1/Main.h
@@ -0,0 +1,11 @@
+
+#ifndef MAIN_H
+#define MAIN_H
+
+//WinMain
+int WINAPI WinMain(	HINSTANCE	hInstance,			//Instance
+					HINSTANCE	hPrevInstance,		//Previous Instance
+					LPSTR		lpCmdLine,			//Command line params
+					int			nShowCmd);			//Window show state
+
+#endif	//MAIN_H

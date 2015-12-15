63f2027109eb8925344f23e36a15fd7da9708449
Veronica159 <2116778705@qq.com>
2015-12-15 09:44:50 +0800

    龚天天第一次作业

---
+++ NB15012龚天天/op1/op1/main.hpp
@@ -0,0 +1,14 @@
+//
+//  main.hpp
+//  op1
+//
+//  Created by 天天 龚 on 15/12/14.
+//  Copyright © 2015年 天天 龚. All rights reserved.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
